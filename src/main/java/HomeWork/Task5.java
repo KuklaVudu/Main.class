7e2b3f5ddce93a15a72b835a2a0907b93b86afa0
Liubov <lyubasik85@rambler.ru>
2023-03-20 18:20:18 +0300

    Homework 5

--- src/main/java/HomeWork/Task5.java
+++ src/main/java/HomeWork/Task5.java
@@ -1,18 +1,36 @@
 package HomeWork;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
-// Реализовать консольное приложение, которое:
-// Принимает от пользователя и “запоминает” строки.
-// Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
-// Если введено revert, удаляет предыдущую введенную строку из памяти.
+
 public class Task5 {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
+
+    // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+    private static void task2() {
+        Random random = new Random();
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            int number = random.nextInt(1, 10);
+            list.add(number);
+        }
+        System.out.println("Список" + "\t->\t" + list);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print(list.getLast() + ", ");
+            list.removeLast();
+        }
+    }
+
+
+    // Реализовать консольное приложение, которое:
+// Принимает от пользователя и “запоминает” строки.
+// Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
+// Если введено revert, удаляет предыдущую введенную строку из памяти.
     private static void task1() {
         Scanner scanner = new Scanner(System.in);
         LinkedList<String> wordsList = new LinkedList<>();
